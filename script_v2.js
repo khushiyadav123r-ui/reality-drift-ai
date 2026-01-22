265ca831075ee37d8b45bf1b8d31c0bf35446a7e
khushiyadav123r-ui <khushiyadav123r@gmail.com>
2026-01-22 23:23:54 +0530

    Update script_v2.js

--- script_v2.js
+++ script_v2.js
@@ -147,4 +147,22 @@ typeEffect();
 // ---------------- ENTER APP FUNCTION ----------------
 function enterApp() {
   document.getElementById("welcomeOverlay").style.display = "none";
+  document.querySelectorAll(".card").forEach(card => {
+  card.addEventListener("mousemove", e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const rotateX = ((y / rect.height) - 0.5) * 10;
+    const rotateY = ((x / rect.width) - 0.5) * -10;
+
+    card.style.transform =
+      `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.03)`;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "rotateX(0) rotateY(0) scale(1)";
+  });
+});
+
 }
